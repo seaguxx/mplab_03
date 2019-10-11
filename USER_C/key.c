03086f00d0e2fb9e424e9f16a9ae0b065a8823a9
H <H@DESKTOP-F55DT6V>
2019-10-11 08:43:10 +0800

    增加按键功能

---
+++ USER_C/key.c
@@ -0,0 +1,23 @@
+#include "../mcc_generated_files/mcc.h"
+#include "../USER_H/key.h"
+#include "../USER_H/lcd.h"
+#include "../AP_H/ap_lcd.h"
+#include "../AP_H/GUI.h"
+char Key1_Fla=0;
+char Key2_Fla=0;
+unsigned int Key1_Cnt=0;
+unsigned int Key2_Cnt=0;
+void Key_Init(void)
+{
+    
+}
+void Key1_Fuction(void)//Mute--Umute
+{
+    Mute
+    
+    Umute
+}
+void Key2_Function(void)//shoot
+{
+
+}
