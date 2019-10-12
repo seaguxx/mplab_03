a75ba0592d90c9e4ccb682fb7f9e691490c2008c
H <H@DESKTOP-F55DT6V>
2019-10-12 14:45:15 +0800

    待解决：数量显示与实际的会有延迟

--- AP_C/GUI.c
+++ AP_C/GUI.c
@@ -2,35 +2,26 @@
 #include "../AP_H/ap_lcd.h"
 #include "../USER_H/lcd.h"
 #include "../mcc_generated_files/mcc.h"
-#define NO1_OK display_char(2,1,4,"一号正常");
-#define NO2_OK display_char(2,5,4,"二号正常");
-#define NO3_OK display_char(1,9,4,"三号正常");
-#define NO4_OK display_char(1,13,4,"四号正常");
 
-#define NO1_Fault display_char(2,1,4,"一号故障");
-#define NO2_Fault display_char(2,5,4,"二号故障");
-#define NO3_Fault display_char(1,9,4,"三号故障");
-#define NO4_Fault display_char(1,13,4,"四号故障");
-
-#define NO1_TNT display_char(2,1,4,"一号释放");
-#define NO2_TNT display_char(2,5,4,"二号释放");
-#define NO3_TNT display_char(1,9,4,"三号释放");
-#define NO4_TNT display_char(1,13,4,"四号释放");
-
-#define Un_White display_char(1,13,4,"        ");
-
-#define Mute    display_char(2,15,2,"消音");
-#define Umute   display_char(2,15,2,"    ");
 void Welcome(void)
 {
     display_char(1,2,6,"干粉控制系统");
     display_char(1,11,4,"中汽客  ");
     display_char(2,9,4,"版本V0.1");
-    Mute
-    __delay_ms(300);
-    Umute
-    __delay_ms(300);
+//    Mute
+//    __delay_ms(300);
+//    Umute
+//    __delay_ms(300);
     
+}
+void Docker(void)
+{
+    display_char(2, 9, 4, "    ");
+    display_char(2,9,3,"状态：");
+    display_char(2,12,1,"0");
+    display_char(2,13,1,"丨");
+    display_char(2,14,1,"4");
+
 }
 void RunGui(void)
 {
