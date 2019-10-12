0d1baec87053844e939811a96747bca916ef741f
H <H@DESKTOP-F55DT6V>
2019-10-12 10:11:40 +0800

    将ECAN.C带的发送函数拆分成TXB0、TXB1、TXB2三个邮箱，发送的时候采用不同的邮箱进行发送，就可以解决之前的问题（定时周期与主程序同时存在发送函数，导致有一个报文的帧类型出错）。

--- mcc_generated_files/tmr0.c
+++ mcc_generated_files/tmr0.c
@@ -19,7 +19,7 @@
     The generated drivers are tested against the following:
         Compiler          :  XC8 2.05 and above
         MPLAB 	          :  MPLAB X 5.20
-*/
+ */
 
 /*
     (c) 2018 Microchip Technology Inc. and its subsidiaries. 
@@ -42,30 +42,31 @@
     CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
     OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
     SOFTWARE.
-*/
+ */
 
 /**
   Section: Included Files
-*/
+ */
 
 #include <xc.h>
 #include "tmr0.h"
 #include "pin_manager.h"
 #include "../USER_H/key.h"
 #include "../AP_H/GUI.h"
+#include "../USER_H/CanBus.h"
 #define MuteKeyDownTime 200 //200*10
 #define ShootKeyDownTime 300
-char Tmr0_Run_Fla=0;
-char Tmr0_1s_Fla=0;
-unsigned int Tmr0_1s_Cnt=0;
+char Tmr0_Run_Fla = 0;
+char Tmr0_1s_Fla = 0;
+char Can_1s_Send_Fla=0;
+unsigned int Tmr0_1s_Cnt = 0;
 /**
   Section: TMR0 APIs
-*/
+ */
 
 void (*TMR0_InterruptHandler)(void);
 
-void TMR0_Initialize(void)
-{
+void TMR0_Initialize(void) {
     // Set TMR0 to the options selected in the User Interface
 
     // T0CS HFINTOSC; T0CKPS 1:64; T0ASYNC synchronised; 
@@ -90,39 +91,30 @@ void TMR0_Initialize(void)
     T0CON0 = 0x83;
 }
 
-void TMR0_StartTimer(void)
-{
+void TMR0_StartTimer(void) {
     // Start the Timer by writing to TMR0ON bit
-    if( Tmr0_Run_Fla==0)
-    {
-        Tmr0_Run_Fla=1;
+    if (Tmr0_Run_Fla == 0) {
+        Tmr0_Run_Fla = 1;
         T0CON0bits.T0EN = 1;
-        PIE3bits.TMR0IE=1;
-    }
-    else
-    {
-        
+        PIE3bits.TMR0IE = 1;
+    } else {
+
     }
 
 }
 
-void TMR0_StopTimer(void)
-{
+void TMR0_StopTimer(void) {
     // Stop the Timer by writing to TMR0ON bit
-    if(Tmr0_Run_Fla==1)
-    {
-        Tmr0_Run_Fla=0;
+    if (Tmr0_Run_Fla == 1) {
+        Tmr0_Run_Fla = 0;
         T0CON0bits.T0EN = 0;
-    }
-    else
-    {
-    
+    } else {
+
     }
 
 }
 
-uint8_t TMR0_ReadTimer(void)
-{
+uint8_t TMR0_ReadTimer(void) {
     uint8_t readVal;
 
     // read Timer0, low register only
@@ -131,16 +123,14 @@ uint8_t TMR0_ReadTimer(void)
     return readVal;
 }
 
-void TMR0_WriteTimer(uint8_t timerVal)
-{
+void TMR0_WriteTimer(uint8_t timerVal) {
     // Write to Timer0 registers, low register only
     TMR0L = timerVal;
- }
+}
 
-void TMR0_Reload(uint8_t periodVal)
-{
-   // Write to Timer0 registers, high register only
-   TMR0H = periodVal;
+void TMR0_Reload(uint8_t periodVal) {
+    // Write to Timer0 registers, high register only
+    TMR0H = periodVal;
 }
 
 void TMR0_ISR(void)//10ms
@@ -148,87 +138,79 @@ void TMR0_ISR(void)//10ms
     // clear the TMR0 interrupt flag
     PIR3bits.TMR0IF = 0;
     Tmr0_1s_Cnt++;
-    if(Tmr0_1s_Cnt==50)
-    {
+    if (Tmr0_1s_Cnt == 50) {
         Yled_Toggle();
+
     }
-    if(Tmr0_1s_Cnt>=100)
-    {
-        Tmr0_1s_Fla=1;
-        Tmr0_1s_Cnt=0;
+    if (Tmr0_1s_Cnt ==100) {
+        Tmr0_1s_Fla = 1;
+        Tmr0_1s_Cnt = 0;
     }
-    if(Tmr0_1s_Fla==1)
-    {
-        Tmr0_1s_Fla=0;
-    }
-    
-    
-    if(Key1_Fla==1)
-    {
-        Fuction_Mute();
-        Key1_Fla=0;
-    }
-    if(Key2_Fla==1)
-    {
-        Function_Shoot();
-        Key2_Fla=0;
-    }
-        if(RB0_GetValue()==0)
-        {
-            Key1_Cnt++;
-            if(Key1_Cnt>=MuteKeyDownTime)
-            {
-                Key1_Fla=1;  
-                Key1_Cnt=0;
-            }
-            else
-            {
-            
-            }
-        }
-        else
-        {
-            Key1_Cnt=0;
-            Key1_Fla=0;
-            //TMR0_StopTimer();
-        }
-    if(RB1_GetValue()==0)
-    {
-        Key2_Cnt++;
-        if(Key2_Cnt>=ShootKeyDownTime)
-        {
-            Key2_Fla=1;
-            Key2_Cnt=0;
-        }
-        else
-        {
+    if (Tmr0_1s_Fla == 1) {
+        Tmr0_1s_Fla = 0;
         
-        }        
+        MessageTform(&Message_Send_Test, 2, 0x00411122, 8, 0xee, 0x01, 0x02, 0x03, 0x04, 0x05, 0x07, 0XEE);
+        CAN_transmit_TXB1(&Message_Send_Test);
+        
+        Can_1s_Send_Fla=1;
     }
-    else
-    {
-        Key2_Fla=0;
-        Key2_Cnt=0;
+
+
+    if (Key1_Fla == 1) {
+        Fuction_Mute();
+        
+        Function_Shoot();
+        
+        Key1_Fla = 0;
+    }
+    if (Key2_Fla == 1) {
+        TMR0_StopTimer();
+        Function_Shoot();
+        TMR0_StartTimer();
+        Key2_Fla = 0;
+    }
+    if (RB0_GetValue() == 0) {
+        Key1_Cnt++;
+        if (Key1_Cnt >= MuteKeyDownTime) {
+            Key1_Fla = 1;
+            Key1_Cnt = 0;
+        } else {
+
+        }
+    } else {
+        Key1_Cnt = 0;
+        Key1_Fla = 0;
         //TMR0_StopTimer();
     }
-    if(TMR0_InterruptHandler)
-    {
+    if (RB1_GetValue() == 0) {
+        Key2_Cnt++;
+        if (Key2_Cnt >= ShootKeyDownTime) {
+            Key2_Fla = 1;
+            Key2_Cnt = 0;
+        } else {
+
+        }
+    } else {
+        Key2_Fla = 0;
+        Key2_Cnt = 0;
+        //TMR0_StopTimer();
+    }
+    if (TMR0_InterruptHandler) {
         TMR0_InterruptHandler();
     }
 
     // add your TMR0 interrupt custom code
 }
 
-
-void TMR0_SetInterruptHandler(void (* InterruptHandler)(void)){
+void TMR0_SetInterruptHandler(void (* InterruptHandler)(void)) {
     TMR0_InterruptHandler = InterruptHandler;
 }
 
-void TMR0_DefaultInterruptHandler(void){
+void TMR0_DefaultInterruptHandler(void) {
     // add your TMR0 interrupt custom code
     // or set custom function using TMR0_SetInterruptHandler()
 }
 
 /**
   End of File
-*/
+ */
